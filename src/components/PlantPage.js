0c4a1cf64459be465b24fa433b821be8c6b491c3
Daria Approvato <dariaapprovato@gmail.com>
2022-12-09 18:30:04 -0500

    commit tree 1facc16eb079a52a110cb6429a4f0cac11da54ca

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      setPlants(data)
+    })
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
       <PlantList />
+      {plants.map((plant) =>
+        <div className="card">
+          <img src={plant.image} />
+          <strong>{plant.name}</strong>
+          <div>{plant.price}</div>
+          {true ? (
+        <button className="primary">In Stock</button>
+      ) : (
+        <button>Out of Stock</button>
+      )}
+        </div>
+      )}
     </main>
   );
 }
